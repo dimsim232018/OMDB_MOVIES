a75c39f27c0d2912c105c9d0a7497a6d076a2f03
SofiaRizou <41473134+SofiaRizou@users.noreply.github.com>
2018-11-18 14:08:10 +0200

    Update header.php

--- header.php
+++ header.php
@@ -11,21 +11,7 @@
 
 <body>
 <?php include 'session.php'; ?>
-<<<<<<< HEAD
-   <nav class="navbar navbar-default">
-       <div class="container">
-          <div class="navbar-header">
-             <a class="nav-brand" href="index.php">WatchMyMovie</a>
-             <?php if (!isset($uname)){?>
-             <a class="nav-brand" href="login.php"><?php echo 'Login';?></a>
-             <?php }else{?>
-             <a class="nav-brand" href="logout.php"><?php echo 'Logout';?></a>
-             <?php };?>
-		  </div>
-       </div>    
-   </nav> 
-=======
-<br>
+
       
  <div class="container">
              <table>                
@@ -45,4 +31,4 @@
           </div>   
      </nav>	
    </div>
->>>>>>> 29278d95f539e0d023cce2cc9911d122288ac441
+
