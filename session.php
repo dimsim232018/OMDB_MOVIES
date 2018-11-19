1c724cb3df163932d2b6c3703e0a33a411506eb8
SofiaRizou <41473134+SofiaRizou@users.noreply.github.com>
2018-11-19 19:55:52 +0200

    Update session.php

--- session.php
+++ session.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
 	//get session variables
-	#if (isset($_SESSION['uid'])){
-	#	$uid=$_SESSION['uid'];
-	#}
+	//if (isset($_SESSION['uid'])){
+	//	$uid=$_SESSION['uid'];
+	//}
 	if (isset($_SESSION['username'])){
 		$username=$_SESSION['username'];
 	}
