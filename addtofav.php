8bcdd96e50c3e24eff3d7de3eb69a3a6ee0a08f9
ttziteihte <jichenyang1995@hotmail.com>
2018-11-24 21:28:51 +0200

    insert constraint

--- addtofav.php
+++ addtofav.php
@@ -8,14 +8,13 @@ $movid=$_REQUEST['imdbID'];
 $genre=$_REQUEST['genre'];
 $title=$_REQUEST['title'];
 $uid=1;
-
 //check if movie already exists in database
-if (!($stmt = $mysql->prepare("select * from movie where userid=? AND movtitle=?"))) {
+if (!($stmt = $mysql->prepare("select movid from movie where userid=? AND movid=?"))) {
 //			 throw new Exception("Prepare failed: (" . $mysql->errno . ") " . $mysql->error);
 			 return false;
 		}
 		// bind params
-		if (!$stmt->bind_param("is", $uid,$title)) {
+		if (!$stmt->bind_param("is", $uid,$movid)) {
 //			throw new Exception("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
 			 return false;
 		}
@@ -28,14 +27,13 @@ if (!($stmt = $mysql->prepare("select * from movie where userid=? AND movtitle=?
 		$result = $stmt->get_result();
         if($result->num_rows === 0) {
         	$sql = "INSERT INTO movie VALUES ($uid,'$movid','$title','$genre')";
-
 			if ($mysql->query($sql) === TRUE) {
     			echo json_encode(['res'=>'New record created successfully']);
 			} else {
     			echo json_encode(['res'=>'Error' . $sql . "<br>" . $mysql->error]);
 			}
 
-        }else{
+        }else if($result->num_rows > 0){
 				echo "Already liked";        
            }
 
