86c29b4b35c2b5f01d68571bd9e5f5a7a9c6fe2c
ttziteihte <jichenyang1995@hotmail.com>
2018-11-24 17:13:58 +0200

    check if record exist before insert

--- addtofav.php
+++ addtofav.php
@@ -7,18 +7,36 @@
 $movid=$_REQUEST['imdbID'];
 $genre=$_REQUEST['genre'];
 $title=$_REQUEST['title'];
+$uid=1;
 
+//check if movie already exists in database
+if (!($stmt = $mysql->prepare("select * from movie where userid=? AND movtitle=?"))) {
+//			 throw new Exception("Prepare failed: (" . $mysql->errno . ") " . $mysql->error);
+			 return false;
+		}
+		// bind params
+		if (!$stmt->bind_param("is", $uid,$title)) {
+//			throw new Exception("Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error);
+			 return false;
+		}
+		// execute 
+		if (!$stmt->execute()) {
+			throw new Exception("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+			 return false;
+		}
+		
+		$result = $stmt->get_result();
+        if($result->num_rows === 0) {
+        	$sql = "INSERT INTO movie VALUES ($uid,'$movid','$title','$genre')";
 
-//must check if movie already exists in database
+			if ($mysql->query($sql) === TRUE) {
+    			echo json_encode(['res'=>'New record created successfully']);
+			} else {
+    			echo json_encode(['res'=>'Error' . $sql . "<br>" . $mysql->error]);
+			}
 
-$sql = "INSERT INTO movie VALUES ($uid,'$movid','$title','$genre')";
-
-if ($mysql->query($sql) === TRUE) {
-    echo json_encode(['res'=>'New record created successfully']);
-} else {
-    echo json_encode(['res'=>'Error' . $sql . "<br>" . $mysql->error]);
-}
-
-
+        }else{
+				echo "Already liked";        
+           }
 
 ?>
