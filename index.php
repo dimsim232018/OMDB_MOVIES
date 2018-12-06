56d7d6c89499d143d8bf29a521d82038e7493209
dimimits <44209019+dimimits@users.noreply.github.com>
2018-12-06 10:39:34 +0200

    UI FX 2
    Added search form effects

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $url=$_SERVER['QUERY_STRING'];
       <div id="welcome" style="color:#fff;">
          <?php if (strcmp($url,'origin=register')==0) {echo "<script>alert('Thank you for registering... You are now logged in!');</script>";} else if (strcmp($url,'origin=logout')==0) {echo "<script>alert ('You are now logged out!');</script>";} ?>
       </div>
-         <div class="jumbotron form">
+         <div class="jumbotron form" id="search_jumbo">
             <h3 class="text-center" style="margin-bottom:50px">Movie Search</h3>
             <form id="searchForm">
                <input type="text" class="form-control" id="searchText"  placeholder="Please enter a movie title and let the magic happen...">
